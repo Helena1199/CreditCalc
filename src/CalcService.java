346e75be334f205a763aa0e8f6f547cb17f4dc24
Helena <v0robjeva.alena@yandex.ru>
2023-01-24 06:41:36 +0300

    CreditCalc

--- src/CalcService.java
+++ src/CalcService.java
@@ -1,8 +1,8 @@
 public class CalcService {
     public int calcPayment(int sum, int month) {
-        double persent = 9.99 / month / 100;
+        double persent = 9.99/100/12;
         double x = Math.pow(1 + persent, month);
-        int payment = (int) (sum * (persent + persent / (x - 1)));
+        int payment = (int) (sum * (persent + persent/ (x - 1)));
         return payment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         CalcService service = new CalcService();
-        int payment = service.calcPayment(1_000_000, 24);
+        int payment = service.calcPayment(1_000_000, 36);
         System.out.println(payment);
     }
 }
